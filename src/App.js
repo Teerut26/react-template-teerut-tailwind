454bd17a89546ebfbf34732fd79eef8db1f935ab
Teerut26 <teerutsr@gmail.com>
2022-01-02 17:25:57 +0700

    1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+import { connect } from "react-redux";
+// var _ = require("lodash");
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col h-screen w-full justify-center items-center">
+      <div className="p-6 w-96 bg-white rounded-xl shadow-lg flex flex-col items-center gap-4">
+        <div className="text-3xl font-bold">{props.NumberManage}</div>
+        <div className="flex gap-3">
+          <div
+            onClick={() => {
+              props.dispatch({
+                type: "SET_NUMBER",
+                playload: props.NumberManage + 1,
+              });
+            }}
+            className="btn btn-primary"
+          >
+            Increase
+          </div>
+          <div
+            onClick={() => {
+              props.dispatch({
+                type: "SET_NUMBER",
+                playload: props.NumberManage - 1,
+              });
+            }}
+            className="btn btn-primary"
+          >
+            Decrease
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default connect((state) => {
+  return state;
+})(App);
